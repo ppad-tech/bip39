e87aeea1da2941cdcaa7f19b46a9ed83a832846f
Jared Tobin <jared@jtobin.io>
2025-02-27 09:39:12 +0400

    test: support ct wordlist

--- test/Main.hs
+++ test/Main.hs
@@ -11,6 +11,10 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Vectors as V
 
+data Wordlist =
+    English
+  | ChineseTraditional
+
 main :: IO ()
 main = do
   vectors_bip39 <- TIO.readFile "etc/vectors.json"
@@ -24,23 +28,31 @@ main = do
 
 bip39_tests :: V.Vectors -> TestTree
 bip39_tests V.Vectors {..} =
-  testGroup "bip39 vectors" $
-    fmap execute v_wordlist
-
-execute :: V.Bip39Test -> TestTree
-execute V.Bip39Test {..} = do
-  let entr = bt_entropy
-      mnem = bt_mnemonic
-      seed = bt_seed
-      xprv = bt_xprv
-      BIP39.Mnemonic out_mnem = BIP39.mnemonic entr
-      out_seed = BIP39.seed out_mnem "TREZOR"
-      out_xprv = case BIP32.master out_seed of
-        Just hd -> BIP32.xprv hd
-        Nothing -> error "bang (bip32)"
-      t_msg = "mnemonic " <> show mnem
-  testGroup t_msg [
-      testCase "mnemonic" $ assertEqual mempty mnem out_mnem
-    , testCase "seed" $ assertEqual mempty seed out_seed
-    , testCase "xprv" $ assertEqual mempty xprv out_xprv
+  testGroup "bip39 vectors" [
+      testGroup "english"
+        (fmap (execute English) v_english)
+    , testGroup "chinese_traditional"
+        (fmap (execute ChineseTraditional) v_chinese_traditional)
     ]
+
+execute :: Wordlist -> V.Bip39Test -> TestTree
+execute wlist V.Bip39Test {..} = do
+    let entr = bt_entropy
+        mnem = bt_mnemonic
+        seed = bt_seed
+        xprv = bt_xprv
+        BIP39.Mnemonic out_mnem = BIP39._mnemonic wl entr
+        out_seed = BIP39.seed out_mnem "TREZOR"
+        out_xprv = case BIP32.master out_seed of
+          Just hd -> BIP32.xprv hd
+          Nothing -> error "bang (bip32)"
+        t_msg = "mnemonic " <> show mnem
+    testGroup t_msg [
+        testCase "mnemonic" $ assertEqual mempty mnem out_mnem
+      , testCase "seed" $ assertEqual mempty seed out_seed
+      , testCase "xprv" $ assertEqual mempty xprv out_xprv
+      ]
+  where
+    wl = case wlist of
+      English -> BIP39.english
+      ChineseTraditional -> BIP39.chinese_traditional
