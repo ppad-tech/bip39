73c7539aa3bf1cba88f059428122450e7104b639
Jared Tobin <jared@jtobin.io>
2025-02-28 07:17:15 +0400

    bench: basic suite

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,4 +1,24 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Criterion.Main
+import Crypto.KDF.BIP39
+
 main :: IO ()
-main = pure ()
+main = defaultMain [
+    bgroup "ppad-bip39" [
+        bench_mnemonic
+      , bench_seed
+    ]
+  ]
+
+bench_mnemonic :: Benchmark
+bench_mnemonic = bench "mnemonic" $ nf mnemonic "my ultra secret entropy!"
+
+bench_seed :: Benchmark
+bench_seed = bench "seed" $ nf (seed mnem) mempty where
+  mnem = "hope simple bubble suggest elbow correct limb hole gloom nasty fringe dolphin finger demand situate unlock junior autumn"
+
